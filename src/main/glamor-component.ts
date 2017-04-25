58e6f038253f04a574fd8906f5fe63bd9cb39c3a
sliv <sliv.mbp@gmail.com>
2017-04-26 06:24:05 +0800

    updated typing of nullIf for latest version of styled component

--- src/main/glamor-component.ts
+++ src/main/glamor-component.ts
@@ -1,12 +1,14 @@
 import { css } from 'glamor';
 import { CSSProperties, SFC } from 'react';
 import StyledTagComponent from 'styled-tag-component';
+
 export type T_GlamorComponent = <P>(a: {
 	tag?: string | any,
 	ep?: string[],
 	rule?: (CSSProperties)[],
 	cn?: (p: P) => (string | [boolean, string])[],
 	style?: (p: P) => Object,
+	nullIf?: (p: P) => boolean,
 }) => SFC<P & { innerRef?: (el: HTMLElement) => void }>;
 export const GlamorComponent: T_GlamorComponent = (a) => {
 	let { rule, cn, ...rest } = a;
